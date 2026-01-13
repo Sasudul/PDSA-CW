cf51b8544cb3a4fafe3592ed17fb289a2b7c0409
Sasudul <sasuduln@gmail.com>
2026-01-13 15:42:36 +0530

    Implement the Back-end Setup Files

---
+++ floodnav-backend/src/main/java/com/floodnav/floodnav_backend/FloodnavBackendApplication.java
@@ -0,0 +1,13 @@
+package com.floodnav.floodnav_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodnavBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodnavBackendApplication.class, args);
+	}
+
+}
---
+++ floodnav-backend/src/test/java/com/floodnav/floodnav_backend/FloodnavBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floodnav.floodnav_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodnavBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
